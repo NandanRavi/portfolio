fdc405e15e21d97135a735da3e2687bb00e2636b
ravi nandan <monubibhu@gmail.com>
2023-06-14 02:28:04 +0530

    3rd commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hvu#&2buh-2l)%3vqo)rwwt^sy(7tt7ax@!ls@4ui)!r!!7trw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = [".vercel.app", '.now.sh']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app", '.now.sh']
 
 
 # Application definition
